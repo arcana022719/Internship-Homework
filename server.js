a9600a85103e2a940ec33003b7cde77a81607c0f
arcana022719 <jakecompendio02271@gmail.com>
2026-09-21 19:09:30 +0800

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ const port = 3000;
 
 
 
+const tasks = [
+    { id: 1, title: "Push to Repository", done: false },
+    { id: 2, title: "Do homework", done: false },
+    { id: 3, title: "Study for exam", done: false }
+];
+
+app.get('/tasks', (req, res) => {
+    res.send(JSON.stringify(task, null, 2));
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find(task => task.id === id)
+
+    if (!task) {
+        return res.status(404).json({
+            message: "Task not found"
+        });
+    }
+
+    res.json(task);
+});
+
 app.get('/', (req, res) => {
     res.json({
         name: "Task API",
